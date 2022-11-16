93213635fa0098e76de9e3f6fe62a4c462205819
korolchuk111 <anna.korolchuk@oa.edu.ua>
2022-11-16 10:23:58 +0200

    main changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,60 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <b-container>
+      <b-form class="text-justify">
+        <b-col sm="7" class="input mx-auto">
+          <myTextarea
+              :maximumLength="maximumLength"
+              @setTextLength="setTextLength"
+          />
+          <myFormLabel
+              :textLength="textLength"
+              :maximumLength="maximumLength"
+              :labelClass="labelClass"
+          />
+        </b-col>
+        <myButton
+          :isDisabled="maximumAchieved"
+        />
+      </b-form>
+    </b-container>
   </div>
 </template>
 
 <script>
+import MyFormLabel from '@/components/home/MyFormLabel.vue';
+import MyButton from '@/components/home/MyButton.vue';
+import MyTextarea from '@/components/home/MyTextarea.vue'
 export default {
+  components: {
+    MyFormLabel,
+    MyButton,
+    MyTextarea
+  },
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data: () => ({
+    maximumLength: 40,
+    warningLength: 30,
+    textLength: 0
+  }),
+  computed: {
+    labelClass: function () {
+      let labelClass = 'success';
+      if (this.textLength > this.maximumLength) {
+        labelClass = 'danger'
+      } else if (this.textLength >= this.warningLength) {
+        labelClass = 'warning'
+      }
+      return labelClass
+    },
+    maximumAchieved: function () {
+      return (this.textLength > this.maximumLength)
+    }
+  },
+  methods: {
+    setTextLength (textLength) {
+      this.textLength = textLength
+    }
   }
 }
 </script>
@@ -56,5 +74,9 @@ li {
 }
 a {
   color: #42b983;
+}
+.input {
+  margin-top: 10px;
+  margin-bottom: 10px;
 }
 </style>
---
+++ src/components/home/MyFormLabel.vue
@@ -0,0 +1,22 @@
+<template>
+    <span :class="'text-' + lableClass">
+      {{ textLength }} / {{ maximumLength }}
+    </span>
+  </template>
+   
+  <script>
+  export default {
+    name: 'MyFormLabel',
+    props: {
+      lableClass: String,
+      maximumLength: Number,
+      textLength: Number
+    }
+  }
+  </script>
+   
+  <style scoped>
+      span {
+          margin-bottom: 4px;
+      }
+  </style>
